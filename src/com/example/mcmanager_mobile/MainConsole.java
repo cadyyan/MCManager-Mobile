bfa5e129a5b8bd5b02b2b83d07b280568769013f
James Durand <cadyyan@Avalon>
2013-01-30 15:04:32 -0600

    Added the WTFPL license to code. All new files MUST have this license.

--- src/com/example/mcmanager_mobile/MainConsole.java
+++ src/com/example/mcmanager_mobile/MainConsole.java
@@ -1,8 +1,23 @@
+/*
+ * DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE Version 2, December 2004
+ * 
+ * Copyright (C) 2004 Sam Hocevar <sam@hocevar.net>
+ * 
+ * Everyone is permitted to copy and distribute verbatim or modified copies of
+ * this license document, and changing it is allowed as long as the name is
+ * changed.
+ * 
+ * DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE TERMS AND CONDITIONS FOR COPYING,
+ * DISTRIBUTION AND MODIFICATION
+ * 
+ * 0. You just DO WHAT THE FUCK YOU WANT TO.
+ */
+
 package com.example.mcmanager_mobile;
 
 import android.app.Activity;
-import android.os.Bundle;
 import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
@@ -13,30 +28,29 @@ public class MainConsole extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
-		Button login = (Button)findViewById(R.id.profile_selector_login);
-		
+
+		Button login = (Button) findViewById(R.id.profile_selector_login);
+
 		login.setOnClickListener(new View.OnClickListener() {
-			
+
 			@Override
 			public void onClick(View v) {
-				Intent intent = new Intent(getBaseContext(), ServerActivity.class);
+				Intent intent = new Intent(getBaseContext(),
+						ServerActivity.class);
 				startActivity(intent);
 			}
 		});
-		
-		
+
 	}
-/////////////////////////////////////////////////////////////////////////////////////
-//HI!!!! (I just like messing with you) - James
-/////////////////////////////////////////////////////////////////////////////////////
+
+	// ///////////////////////////////////////////////////////////////////////////////////
+	// HI!!!! (I just like messing with you) - James
+	// ///////////////////////////////////////////////////////////////////////////////////
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.activity_main, menu);
 		return true;
 	}
-	
-	
 
 }
--- src/com/example/mcmanager_mobile/ServerActivity.java
+++ src/com/example/mcmanager_mobile/ServerActivity.java
@@ -1,38 +1,53 @@
+/*
+ * DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE Version 2, December 2004
+ * 
+ * Copyright (C) 2004 Sam Hocevar <sam@hocevar.net>
+ * 
+ * Everyone is permitted to copy and distribute verbatim or modified copies of
+ * this license document, and changing it is allowed as long as the name is
+ * changed.
+ * 
+ * DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE TERMS AND CONDITIONS FOR COPYING,
+ * DISTRIBUTION AND MODIFICATION
+ * 
+ * 0. You just DO WHAT THE FUCK YOU WANT TO.
+ */
+
 package com.example.mcmanager_mobile;
+
+import android.app.ActionBar;
+import android.app.ActionBar.Tab;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
 
 import com.example.mcmanager_mobile.Listener.AppTabListener;
 
-import android.os.Bundle;
-import android.view.Menu;
-import android.app.ActionBar;
-import android.app.Activity;
-import android.app.ActionBar.Tab;
-
 public class ServerActivity extends Activity {
-	
+
 	@Override
-	public void onCreate(Bundle savedInstanceState){
+	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_control_panel);
-		
+
 		ActionBar abar = getActionBar();
 		abar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
-		
+
 		Tab info = abar.newTab().setText("Info");
 		Tab console = abar.newTab().setText("Console");
 		Tab mods = abar.newTab().setText("Mods");
-		
+
 		info.setTabListener(new AppTabListener(this));
 		console.setTabListener(new AppTabListener(this));
 		mods.setTabListener(new AppTabListener(this));
-		
+
 		abar.addTab(info);
 		abar.addTab(console);
 		abar.addTab(mods);
-		
+
 		abar.selectTab(info);
 	}
-	
+
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
